8a01cfb9b5e2012f7e6377010ff66b337b81426a
Kaushal Ghimire <58645317+kaushal-ghimire@users.noreply.github.com>
2025-06-22 11:43:45 +0545

    Update PaymentProcessingSystemApplication.java

--- src/main/java/com/payment/PaymentProcessingSystem/PaymentProcessingSystemApplication.java
+++ src/main/java/com/payment/PaymentProcessingSystem/PaymentProcessingSystemApplication.java
@@ -3,7 +3,7 @@ package com.payment.PaymentProcessingSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.payment.PaymentProcessingSystem")
 public class PaymentProcessingSystemApplication {
 
 	public static void main(String[] args) {
